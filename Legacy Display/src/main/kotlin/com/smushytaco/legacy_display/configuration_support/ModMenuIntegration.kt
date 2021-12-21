0dde1b951aa0caa4f5597e1bdcb0f751a455e6ea
Nikan Radan <github.com@alias.nikanradan.com>
2021-12-20 22:31:14 -0800

    1.18 Update

--- Legacy Display/src/main/kotlin/com/smushytaco/legacy_display/configuration_support/ModMenuIntegration.kt
+++ Legacy Display/src/main/kotlin/com/smushytaco/legacy_display/configuration_support/ModMenuIntegration.kt
@@ -1,14 +1,11 @@
 package com.smushytaco.legacy_display.configuration_support
-import com.smushytaco.legacy_display.LegacyDisplay
-import io.github.prospector.modmenu.api.ConfigScreenFactory
-import io.github.prospector.modmenu.api.ModMenuApi
+import com.terraformersmc.modmenu.api.ConfigScreenFactory
+import com.terraformersmc.modmenu.api.ModMenuApi
 import me.shedaniel.autoconfig.AutoConfig
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 @Environment(EnvType.CLIENT)
 class ModMenuIntegration: ModMenuApi {
-    override fun getModId() = LegacyDisplay.MOD_ID
-    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> {
-        return ConfigScreenFactory { parent -> AutoConfig.getConfigScreen(ModConfiguration::class.java, parent).get() }
-    }
+    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> =
+        ConfigScreenFactory { parent -> AutoConfig.getConfigScreen(ModConfiguration::class.java, parent).get() }
 }
