3d8eafce618f5f76ddd4c62c043ad7f68a6f4dda
Nikan Radan <github.com@alias.nikanradan.com>
2024-11-25 03:04:20 -0800

    Progress Bar Tweaks

--- Legacy Display/src/main/java/com/smushytaco/legacy_display/mixins/OwoLibCompatibilityMixin.java
+++ Legacy Display/src/main/java/com/smushytaco/legacy_display/mixins/OwoLibCompatibilityMixin.java
@@ -1,17 +1,22 @@
 package com.smushytaco.legacy_display.mixins;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.smushytaco.legacy_display.LegacyDisplay;
 import io.wispforest.owo.config.ui.ConfigScreen;
+import io.wispforest.owo.ui.component.ButtonComponent;
 import io.wispforest.owo.ui.container.FlowLayout;
-import io.wispforest.owo.ui.core.ParentComponent;
+import io.wispforest.owo.ui.core.Component;
 import io.wispforest.owo.ui.core.Surface;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 @Mixin(value = ConfigScreen.class, remap = false)
 public abstract class OwoLibCompatibilityMixin {
-    @WrapOperation(method = "build(Lio/wispforest/owo/ui/container/FlowLayout;)V", at = @At(value = "INVOKE", target = "Lio/wispforest/owo/ui/container/FlowLayout;surface(Lio/wispforest/owo/ui/core/Surface;)Lio/wispforest/owo/ui/core/ParentComponent;"))
-    private ParentComponent hookBuild(FlowLayout instance, Surface surface, Operation<ParentComponent> original) {
-        return original.call(instance, LegacyDisplay.INSTANCE.getConfig().getEnableLegacyDirtScreen() ? LegacyDisplay.INSTANCE.getOWO_LIB_LEGACY_BACKGROUND() : surface);
+    @WrapWithCondition(method = "build(Lio/wispforest/owo/ui/container/FlowLayout;)V", at = @At(value = "INVOKE", target = "Lio/wispforest/owo/ui/container/FlowLayout;surface(Lio/wispforest/owo/ui/core/Surface;)Lio/wispforest/owo/ui/core/ParentComponent;"))
+    private boolean hookBuildOne(FlowLayout instance, Surface surface) { return !LegacyDisplay.INSTANCE.getConfig().getEnableLegacyDirtScreen(); }
+    @WrapOperation(method = "build(Lio/wispforest/owo/ui/container/FlowLayout;)V", at = @At(value = "INVOKE", target = "Lio/wispforest/owo/ui/container/FlowLayout;childById(Ljava/lang/Class;Ljava/lang/String;)Lio/wispforest/owo/ui/core/Component;", ordinal = 1))
+    private Component hookBuildTwo(FlowLayout instance, Class<ButtonComponent> aClass, String s, Operation<Component> original, FlowLayout rootComponent) {
+        if (LegacyDisplay.INSTANCE.getConfig().getEnableLegacyDirtScreen()) rootComponent.surface(LegacyDisplay.INSTANCE.getOWO_LIB_LEGACY_BACKGROUND());
+        return original.call(instance, aClass, s);
     }
 }
--- Legacy Display/src/main/kotlin/com/smushytaco/legacy_display/mixin_logic/LevelLoadingScreenMixinLogic.kt
+++ Legacy Display/src/main/kotlin/com/smushytaco/legacy_display/mixin_logic/LevelLoadingScreenMixinLogic.kt
@@ -3,15 +3,13 @@ import com.smushytaco.legacy_display.LegacyDisplay
 import net.minecraft.client.font.TextRenderer
 import net.minecraft.client.gui.DrawContext
 import net.minecraft.server.WorldGenerationProgressTracker
-import net.minecraft.util.math.MathHelper
 object LevelLoadingScreenMixinLogic {
     fun hookRenderLogic(context: DrawContext, progressProvider: WorldGenerationProgressTracker, width: Int, height: Int, textRenderer: TextRenderer) {
         val i = width / 2
         val j = height / 2 - 9 / 2 - 30
         context.drawCenteredTextWithShadow(textRenderer, "Generating level", i, j + 15, LegacyDisplay.TEXT_COLOR)
         context.drawCenteredTextWithShadow(textRenderer, "Building terrain", i, j + 39, LegacyDisplay.TEXT_COLOR)
-        val progress = MathHelper.clamp(MathHelper.clamp(progressProvider.progressPercentage.toFloat(), 0.0f, 100.0f) / 100.0f * (i + 50.0f), (i - 50).toFloat(), (i + 50).toFloat()).toInt()
         context.fill(i - 50, j + 52, i + 50, j + 54, -0x7f7f80)
-        context.fill(i - 50, j + 52, progress, j + 54, -0x7f0080)
+        context.fill(i - 50, j + 52, i - 50 + (progressProvider.progressPercentage.coerceIn(0, 100) / 100.0 * 100).toInt(), j + 54, -0x7f0080)
     }
 }
