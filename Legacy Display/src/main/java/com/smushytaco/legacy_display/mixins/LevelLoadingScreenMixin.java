1d42b540e3770dd4fdd5472cd203b55fee81de0a
Nikan Radan <github.com@alias.nikanradan.com>
2025-10-09 18:38:07 -0700

    1.21.9

--- Legacy Display/src/main/java/com/smushytaco/legacy_display/mixins/LevelLoadingScreenMixin.java
+++ Legacy Display/src/main/java/com/smushytaco/legacy_display/mixins/LevelLoadingScreenMixin.java
@@ -8,24 +8,24 @@ import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.world.LevelLoadingScreen;
-import net.minecraft.server.WorldGenerationProgressTracker;
 import net.minecraft.text.Text;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.world.chunk.ChunkLoadMap;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 @Mixin(LevelLoadingScreen.class)
 public abstract class LevelLoadingScreenMixin extends Screen {
-    protected LevelLoadingScreenMixin(Text title) { super(title); }
-    @Final
     @Shadow
-    private WorldGenerationProgressTracker progressProvider;
-    @WrapWithCondition(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/world/LevelLoadingScreen;drawChunkMap(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/server/WorldGenerationProgressTracker;IIII)V"))
-    private boolean hookRenderDrawChunkMap(DrawContext context, WorldGenerationProgressTracker progressProvider, int centerX, int centerY, int pixelSize, int pixelMargin) { return !LegacyDisplay.INSTANCE.getConfig().getEnableLegacyLoadingScreen(); }
+    private float loadProgress;
+    protected LevelLoadingScreenMixin(Text title) { super(title); }
+    @WrapWithCondition(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/world/LevelLoadingScreen;drawChunkMap(Lnet/minecraft/client/gui/DrawContext;IIIILnet/minecraft/world/chunk/ChunkLoadMap;)V"))
+    private boolean hookRenderDrawChunkMap(DrawContext context, int centerX, int centerY, int chunkLength, int chunkGap, ChunkLoadMap map) { return !LegacyDisplay.INSTANCE.getConfig().getEnableLegacyLoadingScreen(); }
+    @WrapWithCondition(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/world/LevelLoadingScreen;drawLoadingBar(Lnet/minecraft/client/gui/DrawContext;IIIIF)V"))
+    private boolean hookDrawLoadingBar(LevelLoadingScreen instance, DrawContext context, int x1, int y1, int width, int height, float delta) { return !LegacyDisplay.INSTANCE.getConfig().getEnableLegacyLoadingScreen(); }
     @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawCenteredTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)V"))
     private void hookRender(DrawContext instance, TextRenderer textRenderer, Text text, int centerX, int y, int color, Operation<Void> original) {
         if (LegacyDisplay.INSTANCE.getConfig().getEnableLegacyLoadingScreen()) {
-            LevelLoadingScreenMixinLogic.INSTANCE.hookRenderLogic(instance, progressProvider, width, height, this.textRenderer);
+            LevelLoadingScreenMixinLogic.INSTANCE.hookRenderLogic(instance, loadProgress, width, height, this.textRenderer);
         } else {
             original.call(instance, textRenderer, text, centerX, y, color);
         }
--- Legacy Display/src/main/kotlin/com/smushytaco/legacy_display/mixin_logic/LevelLoadingScreenMixinLogic.kt
+++ Legacy Display/src/main/kotlin/com/smushytaco/legacy_display/mixin_logic/LevelLoadingScreenMixinLogic.kt
@@ -2,14 +2,13 @@ package com.smushytaco.legacy_display.mixin_logic
 import com.smushytaco.legacy_display.LegacyDisplay
 import net.minecraft.client.font.TextRenderer
 import net.minecraft.client.gui.DrawContext
-import net.minecraft.server.WorldGenerationProgressTracker
 object LevelLoadingScreenMixinLogic {
-    fun hookRenderLogic(context: DrawContext, progressProvider: WorldGenerationProgressTracker, width: Int, height: Int, textRenderer: TextRenderer) {
+    fun hookRenderLogic(context: DrawContext, loadProgress: Float, width: Int, height: Int, textRenderer: TextRenderer) {
         val i = width / 2
         val j = height / 2 - 9 / 2 - 30
         context.drawCenteredTextWithShadow(textRenderer, "Generating level", i, j + 15, LegacyDisplay.TEXT_COLOR)
         context.drawCenteredTextWithShadow(textRenderer, "Building terrain", i, j + 39, LegacyDisplay.TEXT_COLOR)
         context.fill(i - 50, j + 52, i + 50, j + 54, -0x7f7f80)
-        context.fill(i - 50, j + 52, i - 50 + (progressProvider.progressPercentage.coerceIn(0, 100) / 100.0 * 100).toInt(), j + 54, -0x7f0080)
+        context.fill(i - 50, j + 52, i - 50 + (loadProgress * 100.0).toInt(), j + 54, -0x7f0080)
     }
 }
